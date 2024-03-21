56d95f25bbc3fa2ffff0998bc0fb988870a1d0c6
omarleee <leighomar12654@gmail.com>
2024-03-21 14:20:24 +0100

    My initial commit

--- eventSync/settings.py
+++ eventSync/settings.py
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'eventSync.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+
+
+
+DATABASES = {
+    'default':
+
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
